31fbea2aaeee9e205f13d7639395544b7bf03f58
Gregory Sebastian Chahuayla Damas <gregorygnr@gmail.com>
2024-06-30 16:21:51 -0500

    corregir

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
-
-const instance = axios.create({
-    baseURL: `${URL}/api`,
-    withCredentials: true,
-});
+/* const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000" */ const instance =
+    axios.create({
+        baseURL: `https://misky-app.onrender.com/api`,
+        withCredentials: true,
+    });
 
 export default instance;
